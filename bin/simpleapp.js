ce431ea497fb4020495d2aa5e83c5eaf51f70ee8
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-10-30 10:19:35 -0400

    simple app

--- bin/simpleapp.js
+++ bin/simpleapp.js
@@ -7,7 +7,7 @@ const app = new cdk.App();
 new AppStack(app, "appStack", {
   /* If you don't specify 'environment', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+   * but a single synthesized template can be deployed anywhere/region. */
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
