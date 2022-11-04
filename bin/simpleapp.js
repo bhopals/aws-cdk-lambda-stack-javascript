79157eb676b2e6c39474dea56aaed42de4dc406f
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-11-04 08:37:09 -0400

    simple app

--- bin/simpleapp.js
+++ bin/simpleapp.js
@@ -8,7 +8,7 @@ new AppStack(app, "appStack", {
   /* If you don't specify 'environment', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere/region/az. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
+  /* Uncomment the next line to specialize this stack for the AWS Accounts
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
