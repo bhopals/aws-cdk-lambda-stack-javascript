5dd220215e36818ddc0c0815d85437799908b596
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-11-05 11:44:33 -0400

    simple app

--- bin/simpleapp.js
+++ bin/simpleapp.js
@@ -10,7 +10,7 @@ new AppStack(app, "appStack", {
    * but a single synthesized template can be deployed anywhere/region/az. */
   /* Uncomment the next line to specialize this stack for the AWS Accounts
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, regions: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
