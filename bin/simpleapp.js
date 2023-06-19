e4873560555762de9756c511b45fa5662992a034
Bhopal Singh <bhopalsinghsis@gmail.com>
2023-06-19 12:09:34 -0400

    Add details in app execute instructions

--- bin/simpleapp.js
+++ bin/simpleapp.js
@@ -13,6 +13,6 @@ new AppStack(app, "appStack", {
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, regions: process.env.CDK_DEFAULT_REGION, process.env.AWS_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '123456789012', region: 'us-east-1', vpc: '', subnet:'' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
